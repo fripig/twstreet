2fc6d44ecb3d822f68831ded7b4d8cf08daa0ff1
fripig <fripig@gmail.com>
2017-07-17 14:25:30 +0800

    fix js

--- javascript/all.js
+++ javascript/all.js
@@ -101,24 +101,24 @@ $(document).ready(function()
     createImage(template,source,x,y,w,h);
   });
 });
-$(window).konami({
-  code : [55,55,55],
-  cheat: function() {
-    $('.banana').slideDown();
-  }
-});
-$(window).konami({
-  code : [54,54,54],
-  cheat: function() {
-    $('h1,#size-slider').delay(100).animate({'opacity':'0'},2900)
-    $('#formbuttons,.template-label').delay(400).animate({'opacity':'0'},2600)
-    $('#settings').delay(1000).animate({'opacity':'0'},2000)
-    $('.left-bottom-corner').delay(800).animate({'opacity':'0'},2200)
-    $('.preview').animate({'top':'-500px','opacity':'0.5'},3000).animate({'width':'0','opacity':'0'},3000,function(){
-      $('#content').slideUp();
-    })
-  }
-});
+// $(window).konami({
+//   code : [55,55,55],
+//   cheat: function() {
+//     $('.banana').slideDown();
+//   }
+// });
+// $(window).konami({
+//   code : [54,54,54],
+//   cheat: function() {
+//     $('h1,#size-slider').delay(100).animate({'opacity':'0'},2900)
+//     $('#formbuttons,.template-label').delay(400).animate({'opacity':'0'},2600)
+//     $('#settings').delay(1000).animate({'opacity':'0'},2000)
+//     $('.left-bottom-corner').delay(800).animate({'opacity':'0'},2200)
+//     $('.preview').animate({'top':'-500px','opacity':'0.5'},3000).animate({'width':'0','opacity':'0'},3000,function(){
+//       $('#content').slideUp();
+//     })
+//   }
+// });
 
 function createImage(template,source,x,y,w,h){
   var cover = new Image();
